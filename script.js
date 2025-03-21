187f8ff1ecb5e2ec518ae49d3aedca8c28c658e7
kimhyeji <gpwl5589@gmail.com>
2025-03-21 12:34:59 +0900

    work

--- script.js
+++ script.js
@@ -1,67 +1,21 @@
 $(document).ready(function () {
   // Initialize Swipers
-  var swiperOne = new Swiper(".one", {
+  var swiperProtein = new Swiper(".swiper", {
     slidesPerView: 3,
-    spaceBetween: 10,
-    loop: true,
+    slidesPerGroup: 1,
+    spaceBetween: 20,
     centeredSlides: true,
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true
-    }
-  });
-
-  var swiperTwo = new Swiper(".two", {
-    slidesPerView: 3,
-    spaceBetween: 10,
-    loop: true,
-    centeredSlides: true,
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true
-    }
-  });
-
-  var swiperThree = new Swiper(".three", {
-    slidesPerView: 3,
-    spaceBetween: 10,
-    loop: true,
-    centeredSlides: true,
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true
-    }
-  });
-
-  var swiperFour = new Swiper(".four", {
-    slidesPerView: 3,
-    spaceBetween: 10,
-    loop: true,
-    centeredSlides: true,
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true
-    }
+    loop: true
   });
 
   // Tab Navigation
-  $(".tab_item").click(function () {
-    var tabIndex = $(this).data("tab");
-    $(".tab_item").removeClass("active");
-    $(this).addClass("active");
-    $(".tab_inner").removeClass("active").addClass("hidden");
-    $(".tab_inner.n" + tabIndex)
-      .removeClass("hidden")
-      .addClass("active");
-
-    if (tabIndex === 1) {
-      swiperOne.update();
-    } else if (tabIndex === 2) {
-      swiperTwo.update();
-    } else if (tabIndex === 3) {
-      swiperThree.update();
-    } else if (tabIndex === 4) {
-      swiperFour.update();
-    }
+  $(function(){
+    $('.tab_inner').hide();
+    $('.tab_item > a').click(function () {
+      $('.tab_inner').hide().filter(this.hash).fadeIn();
+      $('.tab_item > a').removeClass('active');
+      $(this).addClass('active');
+      return false;
+    }).filter(':eq(0)').click();
   });
 });
